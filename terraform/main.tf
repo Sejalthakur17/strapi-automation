60a6f91e43f6fe6897ea70b0cd08fe44576c48b8
Sejal Thakur <sejalthakurcs@gmail.com>
2026-02-16 18:24:15 +0530

    Added Terraform infrastructure and README

--- terraform/main.tf
+++ terraform/main.tf
@@ -58,7 +58,7 @@ resource "aws_security_group" "strapi_sg2" {
 resource "aws_instance" "strapi" {
   ami                    = data.aws_ami.ubuntu.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
+  vpc_security_group_ids = [aws_security_group.strapi_sg2.id]
 
   tags = {
     Name = "strapi-automation"
