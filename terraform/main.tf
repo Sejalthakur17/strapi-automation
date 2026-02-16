3d72b437d2df2bf95ad1a6d2cb9f113ea5d171ac
Sejal Thakur <sejalthakurcs@gmail.com>
2026-02-16 17:37:58 +0530

    Added Terraform infrastructure

---
+++ terraform/main.tf
@@ -0,0 +1,41 @@
+resource "aws_security_group" "strapi_sg" {
+  name = "task6-strapi-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "strapi" {
+  ami           = "ami-0f5ee92e2d63afc18"
+  instance_type = "t2.micro"
+  key_name      = "your-key-name"
+
+  security_groups = [aws_security_group.strapi_sg.name]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              apt update -y
+              apt install docker.io -y
+              systemctl start docker
+              docker pull ${var.docker_username}/task-6:latest
+              docker run -d -p 80:1337 ${var.docker_username}/task-6:latest
+              EOF
+}
