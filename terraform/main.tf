bfa8bc8826f9b74a61ef22f816cc431040bb1db2
Sejal Thakur <sejalthakurcs@gmail.com>
2026-02-16 17:59:35 +0530

    Added Terraform infrastructure

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+# -----------------------------------
+# Fetch Latest Ubuntu 22.04 AMI
+# -----------------------------------
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"] # Canonical (Ubuntu)
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# -----------------------------------
+# Security Group
+# -----------------------------------
 resource "aws_security_group" "strapi_sg" {
-  name = "task6-strapi-sg2"
+  name        = "task6-strapi-sg"
+  description = "Allow SSH and Strapi access"
 
   ingress {
+    description = "SSH"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -9,8 +37,9 @@ resource "aws_security_group" "strapi_sg" {
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    description = "Strapi"
+    from_port   = 1337
+    to_port     = 1337
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -21,21 +50,28 @@ resource "aws_security_group" "strapi_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "task6-strapi-sg"
+  }
 }
 
+# -----------------------------------
+# EC2 Instance
+# -----------------------------------
 resource "aws_instance" "strapi" {
-  ami           = "ami-0f5ee92e2d63afc18"
-  instance_type = "t2.micro"
-  key_name      = "sejal-key"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.strapi_sg.id]
 
-  security_groups = [aws_security_group.strapi_sg.name]
+  tags = {
+    Name = "strapi-automation"
+  }
+}
 
-  user_data = <<-EOF
-              #!/bin/bash
-              apt update -y
-              apt install docker.io -y
-              systemctl start docker
-              docker pull ${var.docker_username}/task-6:latest
-              docker run -d -p 80:1337 ${var.docker_username}/task-6:latest
-              EOF
+# -----------------------------------
+# Output Public IP
+# -----------------------------------
+output "public_ip" {
+  value = aws_instance.strapi.public_ip
 }
